ae6f777cb77396809de955bc9092f5fb9e0b7639
cameronapak <cameronandrewpak@gmail.com>
2025-05-16 08:30:29 -0500

    Refactor App component to extract Header into a separate function, improving code organization and readability. Update navigation links to utilize useLocation for dynamic button styling based on the current route.

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,56 @@ import { AuthButton } from "./AuthButton.tsx";
 import { Form } from "./Form.tsx";
 import VoiceRecorder from "./components/VoiceRecorder";
 import VoiceMessagePlayer from "./components/VoiceMessagePlayer";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import MyVoiceMessages from "./MyVoiceMessages";
 import { Button } from "./components/ui/button.tsx";
 
-function App() {
+function Header() {
+  const location = useLocation();
+  return (
+    <header>
+      <nav className="container max-w-lg mx-auto flex justify-between items-center py-3">
+        <div className="flex items-center">
+          <Link to="/">
+            <Button variant={location.pathname === "/" ? "outline" : "ghost"}>
+              Home
+            </Button>
+          </Link>
+          <Link to="/account">
+            <Button
+              variant={location.pathname === "/account" ? "outline" : "ghost"}
+            >
+              Account
+            </Button>
+          </Link>
+        </div>
+        <AuthButton />
+      </nav>
+    </header>
+  );
+}
 
+function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav className="container max-w-lg mx-auto flex justify-between items-center py-3">
-          <div className="flex items-center">
-            <Link to="/">
-              <Button variant="ghost">Home</Button>
-            </Link>
-            <Link to="/account">
-              <Button variant="ghost">Account</Button>
-            </Link>
-          </div>
-          <AuthButton />
-        </nav>
-      </header>
-      <main className="container mt-16 flex flex-col gap-8">
+      <Header />
+      <main className="container py-12 flex flex-col gap-8">
         <Routes>
-          <Route path="/" element={
-            <>
-              <VoiceRecorder />
-              <MyVoiceMessages />
-            </>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <VoiceRecorder />
+                <MyVoiceMessages />
+              </>
+            }
+          />
           <Route path="/account" element={<Form />} />
           <Route path="/message/:id" element={<VoiceMessagePlayer />} />
         </Routes>
